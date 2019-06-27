abc2b2e0fb58ec05307121726682ff96e441e68e
Devin Walter <walterd04@mounties.mansfield.edu>
2019-06-27 18:22:49 -0400

    add sln for libraries and password encryption library

---
+++ PasswordEncryptor/Interfaces/ISimplePasswordEncryption.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasswordEncryptor.Interfaces
+{
+    public interface ISimplePasswordEncryption<T> where T : class
+    {
+        string HashPassword(T user, string password);
+        PasswordVerificationResult VerifyHashedPassword(T user, string hashedPassword, string providedPassword);
+    }
+}
